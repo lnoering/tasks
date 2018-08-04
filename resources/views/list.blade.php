16b1500707bb9dbce95a5c18bfa87a039ccbf5b5
Leonardo Noering <lnoering@localhost.localdomain>
2018-08-03 23:25:18 -0300

    new - dragable state and task.

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,19 +1,22 @@
 @extends('dashboard')
 
 @section('content')
-    <div id="cols" class="row">
+    <div id="tasks" class="row">
         @foreach($states as $state)
-            <ul class="card col-sm-3">
-                <h1>{{$state->name}}</h1>
-                @foreach($tasks as $key => $task)
-                    @if($task->state_id == $state->id)
-                        <li>
-                            <h3>{{$task->title}}</h3>
-                            <p>{{$task->description}}</p>
-                        </li>
-                        @unset($tasks[$key])
-                    @endif
-                @endforeach
+            <ul class="task-group card col-sm-3">
+                <h1 class="task-group-name">{{$state->name}}</h1>
+                <div class="task-items">
+                    @foreach($tasks as $key => $task)
+                        @if($task->state_id == $state->id)
+                            <li>
+                                <h3>{{$task->title}}</h3>
+                                <p>{{$task->description}}</p>
+                            </li>
+                            @unset($tasks[$key])
+                        @endif
+                            <!-- asauhsahu -->
+                    @endforeach
+                </div>
                 {{ Form::button(__('task.more'),['id'=>'addTask', 'data-id' => $state->id, 'data-url' => URL::to('/').'/task/insert']) }}
             </ul>
         @endforeach
@@ -25,9 +28,18 @@
         </div>
     </div>
     <script type="application/javascript">
-        Sortable.create(cols, {
-            handle: '.card',
-            animation: 150
+        var byId = function (id) { return document.getElementById(id);}
+        Sortable.create(byId('tasks'), {
+            animation: 150,
+            draggable: '.task-group',
+            handle: '.task-group-name'
+        });
+
+        [].forEach.call(byId('tasks').getElementsByClassName('task-items'), function (el){
+            Sortable.create(el, {
+                group: 'task',
+                animation: 150
+            });
         });
     </script>
 @endsection
